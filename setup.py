d1a5430c4f825a586de9f4b663b012bcb3258a12
Char-wow <charlie.brown.1503@gmail.com>
2023-12-06 19:36:42 +0000

    Bot now connects - dont use spyder to run main.py

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 18 19:53:27 2023
+import discord
+import os
+from dotenv import load_dotenv
 
-@author: charl
-"""
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run(token)
