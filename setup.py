6033a70e4c446922ba486267eef7ebb2d60a24ee
Char-wow <charlie.brown.1503@gmail.com>
2024-03-20 18:48:04 +0000

    Added basic SQL connection

--- setup.py
+++ setup.py
@@ -3,13 +3,28 @@ import discord
 import os
 from dotenv import load_dotenv
 
-# import mysql.connector
-# from mysql.connector import Error
-# import pandas as pd
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+pw = os.getenv('SQLROOTPASS')
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -30,3 +45,5 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
 client.run(token)
+
+connection = create_server_connection("localhost", "root", pw)
